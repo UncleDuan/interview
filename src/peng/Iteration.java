880ce91efececf738ac9835f0627b2d41944347b
pengsel <31841637+UncleDuan@users.noreply.github.com>
2019-01-05 18:11:17 +0800

    Add files via upload

---
+++ src/peng/Iteration.java
@@ -0,0 +1,15 @@
+package peng;
+
+import java.util.*;
+
+/**
+ * Created by ionolab-DP on 2018/12/29.
+ */
+public class Iteration {
+
+    Stack<String> s=new Stack<String>();
+
+    public static void main(String[] args) {
+        Set<Integer> set=new HashSet<Integer>();
+    }
+}
